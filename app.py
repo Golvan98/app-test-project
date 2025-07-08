85a3dca563966cf9399ca00f170d10a5fe4dbb09
Golvan98 <gilvinsz@gmail.com>
2025-07-09 04:35:47 +0800

    fix: add missing import to app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory, redirect
 from flask_dance.contrib.google import make_google_blueprint, google
 from flask_cors import CORS
 from dotenv import load_dotenv
